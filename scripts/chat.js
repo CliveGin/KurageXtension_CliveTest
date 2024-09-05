f057da7d7886de332fb048727572044b143d5206
CliveGin <cliveginofficial+git@gmail.com>
2024-09-05 17:53:30 +0100

    Creacion de Menu de Prueba Principal :)

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,9 +3,9 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 $(window).on('load', async function () {
     //This is temporal delay, because for some reason Twitch is slow as shit and overrides the DOM modifications
-    await delay(10000);
+    await delay(5000);
 
-    let $button = $('<img>');
+    let $button = $('<img class="kurage-ico">');
     let $container = $('.chat-input__buttons-container > div:last-child');
     $button.attr('src', chrome.runtime.getURL('medusa_ico.png'));
     $button.css("width", "20px");
